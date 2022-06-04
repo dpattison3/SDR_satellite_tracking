08193d34e55710576229396bf3a5593a23e44e36
Ben Pattison <pattison.ben@gmail.com>
2022-06-04 13:07:37 -0700

    changed path in pll for the build file

--- util/phase_locked_loop.py
+++ util/phase_locked_loop.py
@@ -3,7 +3,9 @@ import numpy as np
 
 
 # This path must be relative to where the python code is executed from.
-c_phase_locked_loop = ctypes.CDLL('build/phase_locked_loop.so')
+c_phase_locked_loop = ctypes.CDLL(
+    "/Users/benjaminpattison/Documents/Projects/satNav/SDR_satellite_tracking/build/phase_locked_loop.so"
+)
 
 
 def cast_list_to_cfloat(input: np.ndarray):
@@ -11,8 +13,13 @@ def cast_list_to_cfloat(input: np.ndarray):
     return (ctypes.c_float * len(contiguous_array)).from_buffer(contiguous_array)
 
 
-def phase_locked_loop(input_signal, sampling_rate, initial_frequency_estimate,
-                      frequency_bandwidth, output_frequency_harmonic=None):
+def phase_locked_loop(
+    input_signal,
+    sampling_rate,
+    initial_frequency_estimate,
+    frequency_bandwidth,
+    output_frequency_harmonic=None,
+):
     real_signal = np.real(input_signal)
 
     real_signal = cast_list_to_cfloat(real_signal)
@@ -28,23 +35,39 @@ def phase_locked_loop(input_signal, sampling_rate, initial_frequency_estimate,
         imaginary_output_harmonic = cast_list_to_cfloat(np.zeros_like(real_signal))
 
         c_phase_locked_loop.PhaseLockedLoopWithHarmonic(
-            len(real_signal), real_signal, imaginary_signal, real_output, imaginary_output,
-            real_output_harmonic, imaginary_output_harmonic, frequencies,
-            ctypes.c_float(sampling_rate), ctypes.c_float(initial_frequency_estimate),
-            ctypes.c_float(frequency_bandwidth),output_frequency_harmonic
+            len(real_signal),
+            real_signal,
+            imaginary_signal,
+            real_output,
+            imaginary_output,
+            real_output_harmonic,
+            imaginary_output_harmonic,
+            frequencies,
+            ctypes.c_float(sampling_rate),
+            ctypes.c_float(initial_frequency_estimate),
+            ctypes.c_float(frequency_bandwidth),
+            output_frequency_harmonic,
         )
     else:
         c_phase_locked_loop.PhaseLockedLoop(
-            len(real_signal), real_signal, imaginary_signal, real_output, imaginary_output,
-            frequencies, ctypes.c_float(sampling_rate), ctypes.c_float(initial_frequency_estimate),
-            ctypes.c_float(frequency_bandwidth)
+            len(real_signal),
+            real_signal,
+            imaginary_signal,
+            real_output,
+            imaginary_output,
+            frequencies,
+            ctypes.c_float(sampling_rate),
+            ctypes.c_float(initial_frequency_estimate),
+            ctypes.c_float(frequency_bandwidth),
         )
 
     output = np.array(real_output) + 1j * np.array(imaginary_output)
     frequencies = np.array(frequencies)
 
     if output_frequency_harmonic:
-        output_harmonic = np.array(real_output_harmonic) + 1j * np.array(imaginary_output_harmonic)
+        output_harmonic = np.array(real_output_harmonic) + 1j * np.array(
+            imaginary_output_harmonic
+        )
     else:
         output_harmonic = None
 
