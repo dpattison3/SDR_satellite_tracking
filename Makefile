bc61e736c47faf2b027e8e967a2ff1884a66c42b
dominic <dominicpattison@gmail.com>
2023-07-09 20:47:01 -0700

    now sounds good but is a mess

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ pll_util:
 	gcc -shared -Wl,-soname,phase_locked_loop -o build/phase_locked_loop.so -fPIC util/phase_locked_loop.c -O3 -ffast-math
 
 sdr_record:
-	g++ util/sdr_record.cc -o build/sdr_record -lSoapySDR
+	g++ util/sdr_record.cc -o build/sdr_record -lSoapySDR -lliquid
--- util/sdr_record.cc
+++ util/sdr_record.cc
@@ -3,6 +3,7 @@
 #include <chrono>
 
 #include <AudioFile.h>
+#include <liquid/liquid.h>
 #include <SoapySDR/Version.hpp>
 #include <SoapySDR/Modules.hpp>
 #include <SoapySDR/Registry.hpp>
@@ -54,10 +55,10 @@ const Configuration DEFAULT_CONFIGURATION = {
         // AGC (automatric gain control) setpoint measured in dBfs.
         {"agc_setpoint", "-30"},
     },
-    .tuned_frequency = 103.7e6,
+    .tuned_frequency = 99.7e6,
     .sample_frequency = 2e6,
     .audio_sample_frequency = 44100,
-    .recording_length = 5,
+    .recording_length = 30,
     .output_audio_file = "data/out_alt.wav",
     .output_iq_file = "data/out_iq.rawiq"
 };
@@ -139,6 +140,10 @@ void RecordFMAudio(SoapySDR::Device* const device, const Configuration& configur
     // the hood.
     void *buffs[1] = { nullptr };
     buffs[0] = malloc(max_buffer_size * 2 * sizeof(float));
+    float filtered_iq[max_buffer_size * 2];
+    float raw_demodulated[max_buffer_size * 2];
+    float filtered_demodulated[max_buffer_size * 2];
+    std::complex<float> filtered_demodulated_complex[max_buffer_size], filtered_demodulated_downsampled[max_buffer_size];
 
     print("Starting recording for ", configuration.recording_length, "seconds with a buffer size of",
           max_buffer_size);
@@ -147,6 +152,43 @@ void RecordFMAudio(SoapySDR::Device* const device, const Configuration& configur
     // We read the device in chunks into the buffer, then process that.
     const size_t num_iterations = static_cast<size_t>(
         std::ceil(configuration.sample_frequency * configuration.recording_length / max_buffer_size)
+    );
+
+    const double stopband_attenuation = 60.0;
+    const unsigned int baseband_filter_coefficient_length = estimate_req_filter_len(
+        /* filter transition */ 0.1, stopband_attenuation
+    );
+    float baseband_filter_coefficients[baseband_filter_coefficient_length];
+    liquid_firdes_kaiser(
+        baseband_filter_coefficient_length, /* frequency cutoff */ (200e3 / configuration.sample_frequency),
+        stopband_attenuation, /* timing offset */ 0.0, baseband_filter_coefficients
+    );
+    firfilt_crcf baseband_filter = firfilt_crcf_create(
+        baseband_filter_coefficients,
+        baseband_filter_coefficient_length
+    );
+
+    const unsigned int audio_filter_coefficient_length = estimate_req_filter_len(
+        /* filter transition */ 0.1, stopband_attenuation
+    );
+    float audio_filter_coefficients[audio_filter_coefficient_length];
+    liquid_firdes_kaiser(
+        audio_filter_coefficient_length, /* frequency cutoff */ (15e3 / configuration.sample_frequency),
+        stopband_attenuation, /* timing offset */ 0.0, audio_filter_coefficients
+    );
+    firfilt_crcf audio_filter = firfilt_crcf_create(
+        audio_filter_coefficients,
+        audio_filter_coefficient_length
+    );
+
+
+    float r=0.117f;     // resampling rate (output/input)
+    float As=60.0f;     // resampling filter stop-band attenuation [dB]
+
+    // create multi-stage arbitrary resampler object
+    msresamp_crcf audio_downsampler = msresamp_crcf_create(
+        configuration.audio_sample_frequency / configuration.sample_frequency,
+        stopband_attenuation
     );
 
     // Annoyingly, the wav file writer used here does not have the capability to write to the file
@@ -159,18 +201,23 @@ void RecordFMAudio(SoapySDR::Device* const device, const Configuration& configur
     )));
 
     // Incrementally write to a binary file for the raw IQ data.
-    auto raw_iq_file = std::fstream(configuration.output_iq_file, std::ios::out | std::ios::binary);
+    auto raw_iq_file = std::fstream("data/raw_iq.rawiq", std::ios::out | std::ios::binary);
+    auto filtered_iq_file = std::fstream("data/filtered_iq.rawiq", std::ios::out | std::ios::binary);
+    auto raw_demodulated_file = std::fstream("data/raw_demodulated.rawiq", std::ios::out | std::ios::binary);
+    auto filtered_demodulated_file = std::fstream("data/filtered_demodulated.rawiq", std::ios::out | std::ios::binary);
 
     // Preallocate loop varialbes.
     long long read_time_ns = 0, previous_read_time_ns = 0;
     int read_stream_return_flags = 0, num_samples_read = 0;
-    std::complex<float> current_raw_iq, previous_raw_iq;
-    float demodulated_value;
+    std::complex<float> current_raw_iq, previous_filtered_iq, current_filtered_iq;
+    std::complex<float> demodulated_value, demodulated_filtered_value;
 
     // Record the start and end times as well as total samples collected. This can indicate if our
     // reading is too slow.
     const std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
     int total_num_samples_read = 0;
+
+    unsigned int num_audio_samples_written = 0;
 
     print("running for", num_iterations, "iterations");
     for (int j = 0; j < num_iterations; j++) {
@@ -194,27 +241,47 @@ void RecordFMAudio(SoapySDR::Device* const device, const Configuration& configur
         float *stream_output = (float *)buffs[0];
 
         if (j == 0) {
-            previous_raw_iq = {stream_output[0], stream_output[1]};
+            previous_filtered_iq = {stream_output[0], stream_output[1]};
         }
 
         for (int i = 0; i < num_samples_read; ++i) {
-            // std:: cout << stream_output[2*i] << " " << stream_output[2*i+1]  << std::endl;
-            // raw_output_data[i] = {stream_output[2*i], stream_output[2*i+1]};
             current_raw_iq = {stream_output[2*i], stream_output[2*i+1]};
 
-            // TODO: filter with liquid_firdes_kaiser
-            // see src/modules/modem/analog/ModemFMStereo.cpp
-            // https://liquidsdr.org/doc/firfilt/
-            // https://liquidsdr.org/doc/firdes/
+            // Filter the raw IQ data.
+            firfilt_crcf_push(baseband_filter, current_raw_iq);    // push input sample
+            firfilt_crcf_execute(baseband_filter, &current_filtered_iq); // compute output
 
-            demodulated_value = std::arg( std::conj(previous_raw_iq) * current_raw_iq );
+            filtered_iq[2*i] = current_filtered_iq.real();
+            filtered_iq[2*i+1] = current_filtered_iq.imag();
 
-            // dumb decimation...
-            // do with https://liquidsdr.org/doc/msresamp/ msresamp_crcf_create
-            if (i % 50 == 0) {
-                audio_output.push_back(demodulated_value);
-            }
-            previous_raw_iq = current_raw_iq;
+            demodulated_value = {std::arg( std::conj(previous_filtered_iq) * current_filtered_iq ) / M_PIf, 0.f};
+            previous_filtered_iq = current_filtered_iq;
+
+            firfilt_crcf_push(audio_filter, demodulated_value);
+            firfilt_crcf_execute(audio_filter, &demodulated_filtered_value);
+
+            raw_demodulated[2*i] = demodulated_value.real();
+            raw_demodulated[2*i+1] = demodulated_value.imag();
+
+            filtered_demodulated[2*i] = demodulated_filtered_value.real();
+            filtered_demodulated[2*i+1] = demodulated_filtered_value.imag();
+
+            filtered_demodulated_complex[i] = demodulated_filtered_value;
+
+        }
+
+        filtered_iq_file.write((char*)filtered_iq, sizeof(float)*2*num_samples_read);
+        filtered_iq_file.seekp(0, std::ios_base::end);
+
+        raw_demodulated_file.write((char*)raw_demodulated, sizeof(float)*2*num_samples_read);
+        raw_demodulated_file.seekp(0, std::ios_base::end);
+
+        filtered_demodulated_file.write((char*)filtered_demodulated, sizeof(float)*2*num_samples_read);
+        filtered_demodulated_file.seekp(0, std::ios_base::end);
+
+        msresamp_crcf_execute(audio_downsampler, filtered_demodulated_complex, num_samples_read, filtered_demodulated_downsampled, &num_audio_samples_written);
+        for (unsigned int output_sample_index = 0; output_sample_index < num_audio_samples_written; ++output_sample_index) {
+            audio_output.push_back(filtered_demodulated_downsampled[output_sample_index].real());
         }
     }
 
@@ -231,6 +298,9 @@ void RecordFMAudio(SoapySDR::Device* const device, const Configuration& configur
     device->closeStream(stream);
     free(buffs[0]);
     raw_iq_file.close();
+    firfilt_crcf_destroy(baseband_filter);
+    firfilt_crcf_destroy(audio_filter);
+    msresamp_crcf_destroy(audio_downsampler);
 
     print("Saving audio file with:", audio_output.size(), "samples.");
     AudioFile<float> wav_file;
